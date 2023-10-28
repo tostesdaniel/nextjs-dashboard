e7552187dcbedcfffe8adf66d7e1c52be0d313c2
Daniel Tostes <tostes.dev@gmail.com>
2023-10-28 18:37:29 -0300

    feat: add revenue chart to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,18 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+import { fetchRevenue } from "../lib/data";
+import RevenueChart from "../ui/dashboard/revenue-chart";
+import { lusitana } from "../ui/fonts";
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+      </div>
+    </main>
+  );
 }
